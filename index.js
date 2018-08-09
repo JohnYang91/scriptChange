2ac160ccdae9967dea73db7cfd93cff3f6b7e6dd
JohnYang91 <jy.0116@gmail.com>
2018-08-09 12:50:14 -0700

    Prompt Question

--- index.js
+++ index.js
@@ -1,45 +1,51 @@
 var fs = require('fs')
+const readline = require('readline')
 
-var fileName = "With Arms Wide Open (Needs Carrots).lrc"
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
-var file = fs.readFileSync(`${fileName}`, 'utf8')
+rl.question("What is the name of your file? (Include Caps, Spaces and Tag (ex. Hello World.lrc)): ", (answer) => {
 
-var lines = file.split('\n');
-var newFile;
+    console.log(`Your Filename: ${answer}`)
+    console.log("Creating...")
 
-//Change Line to 5
-for (var line = 0; line < lines.length; line++){
-    if(lines[line].includes("[")) {
-        var bracket = lines[line].slice(0, 10);
-        var changeFirst = bracket.replace('[', '<')
-        var changeSecond = changeFirst.replace(']', '>')
+    var fileName = answer
 
-        var newTag = bracket+changeSecond
-        var slicedLine = lines[line].slice(10)
-        var newLine = newTag+slicedLine
-
-        newFile = newFile + newLine
+    try {
+    var file = fs.readFileSync(`${fileName}`, 'utf8')
+    } catch (error) {
+        console.log("The file does not exit!")
     }
-}
+    
 
-setTimeout(() => {
-    fs.writeFile("New-"+`${fileName}`, newFile.slice(9), function(err) {
-        if(err) {
-            return console.log(err);
+    var lines = file.split('\n');
+    var newFile;
+
+    //Change Line to 5
+    for (var line = 0; line < lines.length; line++){
+        if(lines[line].includes("[")) {
+            var bracket = lines[line].slice(0, 10);
+            var changeFirst = bracket.replace('[', '<')
+            var changeSecond = changeFirst.replace(']', '>')
+
+            var newTag = bracket+changeSecond
+            var slicedLine = lines[line].slice(10)
+            var newLine = newTag+slicedLine
+
+            newFile = newFile + newLine
         }
-        console.log("The file was saved!")
-    })
-}, 5000)
+    }
 
-// const readline = require('readline')
+    setTimeout(() => {
+        fs.writeFile("New-"+`${fileName}`, newFile.slice(9), function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!")
+        })
+    }, 5000)
 
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// })
-
-// rl.question("What do you think of Node?", (answer) => {
-//     console.log(`Thank you for your valuable feedback: ${answer}`)
-
-//     rl.close();
-// })
+rl.close();
+})
